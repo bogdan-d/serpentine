0c35745af289f4a3eee1afb93e18a0ca805bde99
BogdanD <bogdan.dinu@merchantpro.ro>
2026-01-03 02:59:21 +0200

    feat(bazzite-hardware-setup): ✨ port upstream bazzite changes
    * Incremented `HWS_VER` from 65 to 68.
    * Enhanced kernel argument checks for IOMMU changes.
    * Added a new fixup for IWD configuration to manage Wi-Fi networks.
    * Added bees deduplication tool in bazzite apps recipe.

--- system_files/usr/share/serpentine/zen-config/01-serpentine-global.js
+++ system_files/usr/share/serpentine/zen-config/01-serpentine-global.js
@@ -1,5 +1,7 @@
 // Serpentine Global
-pref("media.ffmpeg.vaapi.enabled", true);
+pref("gfx.webrender.all", true);
+// pref("media.ffmpeg.vaapi.enabled", true);
+pref("media.hardware-video-decoding.force-enabled", true);
 // pref("reader.parse-on-load.enabled", false);
 // pref("media.webspeech.synth.enabled", false);
 // pref("browser.tabs.groups.smart.enabled", false);
